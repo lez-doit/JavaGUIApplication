a06ac248273dae4bcdb1a7bd8c532913d961a9ff
Artem Popov <br4v0.2403@gmail.com>
2023-01-26 11:30:50 +0200

    Added label with icon to frame

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
         MyFrame myFrame = new MyFrame();
+
+        JLabel label = new JLabel();
+        label.setText("This is text of label"); //set text of label
+        myFrame.add(label);
+        ImageIcon image1 = new ImageIcon("image1.png");
+        label.setIcon(image1);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
     }
 }
