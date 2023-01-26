41a6f53808edfd9661be9c1ebf183b321ecd1131
Artem Popov <br4v0.2403@gmail.com>
2023-01-26 11:09:41 +0200

    Added logo to program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame Title");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+        frame.setResizable(false); //prevent from resizing
+        frame.setSize(600, 600);
+        frame.setVisible(true); //making frame visible
+
+        ImageIcon image = new ImageIcon("icon.png");
+        frame.setIconImage(image.getImage()); //added logo
     }
 }
